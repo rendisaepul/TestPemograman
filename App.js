0decc39e69ec09bf43233d9ca2ba9be5063127bb
rendisaepulbahri <rendisaepulbahri09@gmail.com>
2018-01-29 15:28:31 +0700

    fix react-redux

--- App.js
+++ App.js
@@ -1,32 +1,18 @@
-import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
-import { Provider } from 'react-redux'
+import React from 'react';
+import { Provider } from 'react-redux';
 
-import fetchUserReducer from './src/reducer'
-import FetchUser from './src/index';
+import configureStore from './configureStore';
+import Photos from './src/index';
 
-import mySaga from './sagas'
-
-// create the saga middleware
-const sagaMiddleware = createSagaMiddleware()
-// mount it on the Store
-const store = createStore(
-  fetchUserReducer,
-  applyMiddleware(sagaMiddleware)
-)
-
-// then run the saga
-sagaMiddleware.run(mySaga)
-
-// render the application
+const store = configureStore();
 
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <FetchUser/>
+        <Photos />
       </Provider>
-    );
+    )
   }
 }
 
