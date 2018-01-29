e17d9cb7f3ce2c4f0396917c8ba001028552f95c
rendisaepulbahri <rendisaepulbahri09@gmail.com>
2018-01-29 15:31:08 +0700

    create listview

--- src/index.js
+++ src/index.js
@@ -5,62 +5,59 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Button
-} from 'react-native';
+import { View } from 'react-native';
+import { fetchPhotos } from './actions';
 import { connect } from 'react-redux';
+import { Button, Content, CardItem, Container, Text, List, ListItem, Left, Right, Body, Thumbnail } from 'native-base';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-export default class FetchUser extends Component<{}> {
+class Photos extends Component {
+
+  getPhotos = () => {
+    this.props.fetchPhotos();
+  }
 
   render() {
+    const isFetching = this.props.appData.isFetching;
     return (
-      <View style={styles.container}>
-        <Button
-          onPress={() => {this.fetchJsonPlaceHolder()}}
-          title='Fetch'
-        />
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Container>
+        <View style={{ alignItems: 'center' }}>
+          <Text style={{ fontSize: 25 }}>REDUX SAGA EXAMPLES</Text>
+          <Button style={{ alignSelf: 'center', marginTop: 10 }} onPress={() => { this.getPhotos() }}>
+            <Text>{isFetching ? ('Loading...') : ('Load Data')}</Text>
+          </Button>
+        </View>
+        <View style={{ flex: 1, marginTop: 15, backgroundColor: 'rgba(0,0,0,0.03)' }}>
+          <Content>
+            <List
+              dataArray={this.props.appData.data}
+              renderRow={(item) =>
+              <ListItem avatar>
+                <Left>
+                  <Thumbnail source={{ uri: item.thumbnailUrl }} />
+                </Left>
+                <Body>
+                  <Text>{ item.title }</Text>
+                </Body>
+              </ListItem>}>
+            </List>
+          </Content>
+        </View>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+function mapStateToProps (state) {
+  return {
+    appData: state.appData
+  }
+}
 
-export default connect(mapStateToProps, actions)(FetchUser);
+function mapDispatchToProps (dispatch) {
+  return {
+    fetchPhotos: () => dispatch(fetchPhotos())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Photos);
